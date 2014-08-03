d067fe8877c148f027ed94b86538d4abac529ab6
Nikhil Srinivasan <nikhil@nvs.io>
2014-08-03 10:42:01 -0700

    initial commit, add wit pod

---
+++ ios/wit.ai/wit.ai/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  wit.ai
+//
+//  Created by Nikhil Srinivasan on 8/3/14.
+//  Copyright (c) 2014 Bitpass, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ios/wit.ai/wit.ai/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  wit.ai
+//
+//  Created by Nikhil Srinivasan on 8/3/14.
+//  Copyright (c) 2014 Bitpass, Inc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
