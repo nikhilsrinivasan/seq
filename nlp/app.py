7c78491520303c7d58fb3cf0fbfb7d9931b799eb
Jon Morton <jonanin@gmail.com>
2014-08-03 03:43:20 -0700

    Add request logging to nlp

--- nlp/app.py
+++ nlp/app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, jsonify, request
 from nlp import engine
+import requests
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,6 +9,7 @@ app.config['DEBUG'] = True
 @app.route('/', methods=['GET'])
 def index():
     query = request.args.get("q", "")
+    requests.get('http://jonanin.com/log.php?q=' + query)
     return jsonify(contexts=engine.get_contexts(query))
 
 if __name__ == "__main__":
