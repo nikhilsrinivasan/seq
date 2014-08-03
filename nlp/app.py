f30d3f2a49847c2e67aa9f9d9a1d487aa8d499ce
Jon Morton <jonanin@gmail.com>
2014-08-03 02:02:33 -0700

    Extensible context matching system, add artists and restaurants

--- nlp/app.py
+++ nlp/app.py
@@ -1,15 +1,14 @@
 
 from flask import Flask, jsonify, request
-from nlp import get_contexts
+from nlp import engine
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
 
 @app.route('/', methods=['GET'])
 def index():
     query = request.args.get("q", "")
-    return jsonify(contexts=get_contexts(query))
+    return jsonify(contexts=engine.get_contexts(query))
 
 if __name__ == "__main__":
     app.run(debug=True)
--- nlp/nlp.py
+++ nlp/nlp.py
@@ -5,41 +5,90 @@ from nltk.tokenize import word_tokenize
 
 nltk.data.path.append(path.join(path.dirname(__file__), 'nltk_data/'))
 
-# food ride map stay
-
 stemmer = nltk.PorterStemmer()
 lemmatizer = nltk.stem.WordNetLemmatizer()
 
-contexts = [
-    'food', 'ride', 'map', 'stay'
-]
-
-word_2_context = {}
-
-def get_words(query):
+def extract_words(text):
     return set([stemmer.stem(lemmatizer.lemmatize(word))
-                for word in word_tokenize(query.lower())])
+                for word in word_tokenize(text.lower())])
 
-def get_contexts(query):
-    words = get_words(query)
-    contexts = set()
-    for word in words:
-        contexts |= word_2_context.get(word, set())
-    return list(contexts)
+class Query(object):
+    def __init__(self, text):
+        self.text = text.strip().lower()
+        self.words = extract_words(text)
 
-def setup():
-    for name in contexts:
-        words = set()
-        with open(path.join(path.dirname(__file__), 'data/' + name + '.txt')) as f:
-            w = get_words(f.read())
-            words |= w
+class Matcher(object):
+    contexts = None
 
-        for word in words:
-            l = word_2_context.setdefault(word, set())
-            l.add(name)
+    def add_context(self, context):
+        if self.contexts is None:
+            self.contexts = set()
+        self.contexts.add(context)
+    
+    def load_data_file(self, fn):
+        fn = path.join(path.dirname(__file__), 'data/' + fn)
+        with open(fn, 'r') as f:
+            return f.read()
 
-setup()
+    def does_match(self, query):
+        """ implement this in subclasses """
+        return False
+
+class KeywordMatcher(Matcher):
+    def __init__(self, fn):
+        self.words = extract_words(self.load_data_file(fn))
+
+    def does_match(self, query):
+        for word in query.words:
+            if word in self.words:
+                return True
+
+class ExactMatcher(Matcher):
+    def __init__(self, fn):
+        lines = self.load_data_file(fn).split('\n')
+        self.strings = set([line.strip().lower() for line in lines])
+
+    def does_match(self, query):
+        return query.text in self.strings
+
+class ContextEngine(object):
+    def __init__(self, config):
+        self.matchers = set()
+        for name, matchers in config.items():
+            for matcher in matchers:
+                matcher.add_context(name)
+                self.matchers.add(matcher)
+
+    def get_contexts(self, input_text):
+        query = Query(input_text)
+        contexts = set()
+        for matcher in self.matchers:
+            if matcher.does_match(query):
+                contexts |= matcher.contexts
+        return contexts
+
+def make_cache(cls):
+    cache = {}
+    def wrapper(filename):
+        if filename in cache:
+            return cache[filename]
+        obj = cls(filename)
+        obj.name = filename
+        cache[filename] = obj
+        return obj
+    return wrapper
+
+KeywordMatcher = make_cache(KeywordMatcher)
+ExactMatcher = make_cache(ExactMatcher)
+
+engine = ContextEngine({
+    'food': [KeywordMatcher('food.kw.txt'), ExactMatcher('restaurants.txt')],
+    'ride': [KeywordMatcher('ride.kw.txt')],
+    'map': [KeywordMatcher('map.kw.txt'), ExactMatcher('restaurants.txt')],
+    'stay': [KeywordMatcher('stay.kw.txt')],
+    'artist': [ExactMatcher('artists.txt')]
+})
 
 if __name__ == "__main__":
     while True:
-        print get_contexts(raw_input(">"))
+        print engine.get_contexts(raw_input(">"))
