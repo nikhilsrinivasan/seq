90ac73b4f1587be102ea3059d823cc7caccdf628
Jon Morton <jonanin@gmail.com>
2014-08-03 08:45:37 -0700

    Uber silently fails

--- nlp/app.py
+++ nlp/app.py
@@ -10,9 +10,11 @@ app = Flask(__name__)
 uber_user = os.environ['UBER_USERNAME']
 uber_pass = os.environ['UBER_PASSWORD']
 
-uber_client = UberClient(uber_user,
-                         UberClient.login(uber_user, uber_pass))
-
+try:
+    uber_client = UberClient(uber_user,
+                             UberClient.login(uber_user, uber_pass))
+except:
+    pass
 
 @app.route('/')
 def index():
@@ -22,16 +24,19 @@ def index():
 
 @app.route('/nearest_ubers')
 def nearest_ubers():
+
+    if not uber_client:
+        return jsonify(eat=None)
+        
     class Loc(object):
         def __init__(self, lat, lon):
             self.latitude = lat
             self.longitude = lon
-              
+
     loc = Loc(request.args.get("latitude", None),
               request.args.get("longitude", None))
 
     state = uber_client.ping(loc)
-
     min_eta = min(v.min_eta for k, v in state.nearby_vehicles.items() if v.min_eta is not None)
 
     return jsonify(eta=min_eta)
