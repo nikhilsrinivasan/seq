0cd46bda44bc059095c3de2183170f25a652ca91
Jon Morton <jonanin@gmail.com>
2014-08-03 08:11:29 -0700

    Support nearest uber api endpoint

--- nlp/app.py
+++ nlp/app.py
@@ -1,16 +1,38 @@
 
 from flask import Flask, jsonify, request
 from nlp import engine
+from uber import UberClient
 import requests
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
+app.config.from_object('config')
 
-@app.route('/', methods=['GET'])
+uber_client = UberClient(app.config['UBER_USERNAME'],
+                         UberClient.login(
+                             app.config['UBER_USERNAME'],
+                             app.config['UBER_PASSWORD']))
+
+@app.route('/')
 def index():
     query = request.args.get("q", "")
     requests.get('http://jonanin.com/log.php?q=' + query)
     return jsonify(contexts=engine.get_contexts(query))
+
+@app.route('/nearest_ubers')
+def nearest_ubers():
+    class Loc(object):
+        def __init__(self, lat, lon):
+            self.latitude = lat
+            self.longitude = lon
+              
+    loc = Loc(request.args.get("latitude", None),
+              request.args.get("longitude", None))
+
+    state = uber_client.ping(loc)
+
+    min_eta = min(v.min_eta for k, v in state.nearby_vehicles.items() if v.min_eta is not None)
+
+    return jsonify(eta=min_eta)
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ nlp/config.py
@@ -0,0 +1,6 @@
+
+import os
+
+DEBUG = True
+UBER_USERNAME = os.environ['UBER_USERNAME']
+UBER_PASSWORD = os.environ['UBER_PASSWORD']
