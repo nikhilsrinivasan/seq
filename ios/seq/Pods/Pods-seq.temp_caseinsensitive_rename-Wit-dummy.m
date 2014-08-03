1501e632c23940ef4e09ef4d0662c6b2d2e79752
Nikhil Srinivasan <nikhil@nvs.io>
2014-08-03 13:08:04 -0700

    fix on fix on fix

--- ios/seq/Pods/Pods-seq.temp_caseinsensitive_rename-Wit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_seq_temp_caseinsensitive_rename_Wit : NSObject
-@end
-@implementation PodsDummy_Pods_seq_temp_caseinsensitive_rename_Wit
-@end
--- ios/seq/Pods/Pods-seq.temp_caseinsensitive_rename-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_seq_temp_caseinsensitive_rename : NSObject
-@end
-@implementation PodsDummy_Pods_seq_temp_caseinsensitive_rename
-@end
