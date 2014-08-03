6d210528f4504fc2cd6d508c9d0105ba6699281f
Jon Morton <jonanin@gmail.com>
2014-08-02 20:16:43 -0700

    Add test queries file

---
+++ nlp/test_queries.py
@@ -0,0 +1,9 @@
+
+# query -> expected contexts
+
+test_queries = {
+    "breakfast in soma": ['food'],
+    "ride to castro street": ['ride'],
+    "335 Pioneer Way": ['maps', 'uber'],
+    "italian": ['food'],
+}
