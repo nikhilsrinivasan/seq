554c769464208cbd3546f0e9b5a042bc808d3f6a
Nikhil Srinivasan <nikhil@nvs.io>
2014-08-02 18:30:32 -0700

    initial commit

--- ios/seq/seq/ViewController.h
+++ ios/seq/seq/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
