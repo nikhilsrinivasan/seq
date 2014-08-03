fa4270e98c013ad6b0d7e2de226b449fff3ef3a2
Nikhil Srinivasan <nikhil@nvs.io>
2014-08-03 13:27:25 -0700

    add site

---
+++ js/main.js
@@ -0,0 +1,30 @@
+(function() {
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+        'timeStamp', 'trace', 'warn'
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
+    }
+}());
+
+$(function(){
+	
+	var h = $(window).height();
+	$('body').height(h);
+	$('#iphone').height(Math.round(h*0.9));
+	$('#iphone').transition({marginBottom:"-30%",opacity:1},800);
+	$('.main').transition({marginTop:"150px",opacity:1},800);
+});
